1e7008171e731c3f4c37f02337720d0bcec404e7
Yacun Wang <colts16661@gmail.com>
2022-11-30 16:26:05 -0800

    free data space

--- run.py
+++ run.py
@@ -20,14 +20,14 @@ def parse() -> argparse.Namespace:
     )
     parser.add_argument("-d", "--data", type=str, help="data path", default='nyt/coarse')
     parser.add_argument(
-        "-m", "--model", type=str, nargs='+', choices=['tfidf', 'w2v'], 
+        "-m", "--model", type=str, nargs='+', choices=['tfidf', 'w2v'],
         help="models to run", action='extend', default=['tfidf', 'w2v']
     )
     parser.add_argument("-s", "--stem", action='store_true', help="only used in experiments")
-    
+
     parser.add_argument("-o", "--output", action='store_true', help="result filepath, only used in experiments")
     parser.add_argument(
-        "-p", "--plot", action='store_true', 
+        "-p", "--plot", action='store_true',
         help="visualize document length distribution, only used in experiments"
     )
     return parser.parse_args()
@@ -39,12 +39,12 @@ def test() -> None:
     """
     print('Running Test Data Target:')
     d = Data(
-        data_dir='test/', 
-        dataset='testdata', 
-        stem=False, 
+        data_dir='test/',
+        dataset='testdata',
+        stem=False,
         special_tokens=True
     )
-    
+
     Tfidf_Model.run(d)
     config = load_config('config/test_w2v_config.json')
     Word2Vec_Model.run(d, **config)
@@ -53,9 +53,9 @@ def test() -> None:
 def experiment(dataset: str, stem, models: list, viz=False) -> None:
     print('Running Experiment Target:')
     d = Data(
-        data_dir='data/', 
-        dataset=dataset, 
-        stem=stem, 
+        data_dir='data/',
+        dataset=dataset,
+        stem=stem,
         special_tokens=True
     )
 
@@ -68,7 +68,7 @@ def experiment(dataset: str, stem, models: list, viz=False) -> None:
         tfidf_result = Tfidf_Model.run(d, **config)
         if args.output:
             write_result('results/tfidf_runs.json', **tfidf_result)
-    
+
     # run Word2Vec Model
     if 'w2v' in models:
         config = load_config('config/exp_w2v_config.json')
@@ -90,14 +90,15 @@ def tune(dataset: str, models: list) -> None:
             # load
             one_config = dict(zip(keys, bundle))
             d = Data(
-                data_dir='data/', 
-                dataset=dataset, 
+                data_dir='data/',
+                dataset=dataset,
                 stem=one_config['stem'],
                 special_tokens=True
             )
             one_tfidf_result = flatten_dict(Tfidf_Model.run(d, **one_config))
             combine_dict(tfidf_result, one_tfidf_result)
-        
+            del d
+
         write_result('results/tfidf_ht.json', **tfidf_result)
 
     # run Word2Vec Model
@@ -111,27 +112,28 @@ def tune(dataset: str, models: list) -> None:
             # load
             one_config = {'model_params': dict(zip(keys, bundle))}
             d = Data(
-                data_dir='data/', 
-                dataset=dataset, 
+                data_dir='data/',
+                dataset=dataset,
                 stem=False if dataset == '20news/fine' else True,
                 special_tokens=True
             )
             one_w2v_result, model = Word2Vec_Model.run(d, **one_config)
             one_w2v_result = flatten_dict(one_w2v_result)
             combine_dict(w2v_result, one_w2v_result)
-            
+            del d
+
             # check best
             if one_w2v_result['macro_f1'] > best_macro:
                 best_model, best_macro, best_config = (
                     model, one_w2v_result['macro_f1'], one_config['model_params']
                 )
-        
+
         best_str = f'best_{dataset.replace("/", "_")}'
         best_model.save(f'models/new_ht/{best_str}.model')
         with open(f'models/new_ht/{best_str}_config.json', 'w') as f:
             json.dump(best_config, f)
         write_result('results/w2v_ht.json', **w2v_result)
-            
+
 
 if __name__ == "__main__":
     # parse command-line arguments
@@ -140,7 +142,7 @@ if __name__ == "__main__":
     # test target
     if args.target == 'test':
         test()
-    
+
     else:
         # load data
         models = list(set(args.model))
@@ -152,4 +154,3 @@ if __name__ == "__main__":
         # hyperparameter tuning target
         elif args.target in ['hyperparameter', 'ht']:
             tune(args.data, models)
-            
